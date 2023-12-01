9379a64b5a55a6ae575bbf32d90b039d45cfee0b
JihadulSijan <jihadulabedin783@gmail.com>
2023-12-01 02:36:57 -1200

    order-summery-fuctionality-added

--- src/components/shop/shop Combiner/ShopCombiner.jsx
+++ src/components/shop/shop Combiner/ShopCombiner.jsx
@@ -9,20 +9,47 @@ const ShopCombiner = () => {
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [])
-    
+
+    // State for set an array for storing items object
      const [cartArray,setCartArray] = useState([])
      const addToCartEventHandler = (val)=>{
         const newCartArray = [...cartArray,val]
         setCartArray(newCartArray)
     }
-    // console.log(product)
+
+    // state for calculation
+    const priceArray = []
+    cartArray.map((val)=>{
+        priceArray.push(val.price)
+    })
+    const total = priceArray.reduce((val1,val2)=>{
+       return val1+val2
+   },0)
+
+    // clear ShoopItem
+    const clearItems = ()=>{
+        setCartArray([])
+    }
+
+    // shipping charge
+    let shipping = 0;
+    if(cartArray.length !== 0){
+        shipping = 10
+    }
+    
+    // Tax Calculation
+    const tax = (total*5)/100;
+
+    // Grand Total
+    const grandTotal = (total+tax+shipping)
+
     return (
         <div className="shopCombiner container mx-auto">
             <div className="shopItem-wrapper">
                 {
                     product.map(val => {
                         
-                        return <ShoopItem key={val.id} productObj={val} addToCart={addToCartEventHandler}></ShoopItem>
+                        return <ShoopItem key={val.id} productObj={val} addToCart={()=>addToCartEventHandler(val)}></ShoopItem>
 
                     })
                 }
@@ -33,14 +60,14 @@ const ShopCombiner = () => {
                     <div className="summeryReport">
                         <ul>
                             <li>Selected Items: {cartArray.length}</li>
-                            <li>Total Price:</li>
-                            <li>Total Shopping Charges</li>
-                            <li>Tax: $</li>
-                            <li>Grand Total: $</li>
+                            <li>Total Price: ${total}</li>
+                            <li>Total Shopping Charges: ${shipping}</li>
+                            <li>Tax: ${tax}</li>
+                            <li>Grand Total: ${grandTotal}</li>
                         </ul>
                     </div>
                     <div className="cartButtons">
-                        <button id="btnClearCart">Clear Cart</button>
+                        <button onClick={clearItems} id="btnClearCart">Clear Cart</button>
                         <button id="btnReviewOrder">Review Order</button>
                     </div>
                 </div>
